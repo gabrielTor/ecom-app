431d29f0e83462bf84b324ffa229af9c5d788ecb
gabrielTor <gabriel_torres2012@yahoo.com>
2022-11-16 15:29:55 -0300

    spinner on products load

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Button, Center, Heading, Text } from '@chakra-ui/react'
+import { Box, Flex, Image, Button, Center, Heading, Spinner } from '@chakra-ui/react'
 import Card from './Card'
 import { useGetProductsQuery } from '../../Redux/apiSlice'
 
@@ -7,24 +7,30 @@ export default function Home() {
     const {
         data,
         isLoading,
-        isSuccess,
+        // isSuccess,
         isError,
         error
     } = useGetProductsQuery()
 
-    if(isLoading) (<Text>Loading...</Text>)
-
     if(isError) console.log(error)
 
-    if(isSuccess){
-
-        return (
+    return (
+    <>{isLoading ?
+        <Center m='30%'>
+            <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='green.500'
+                size='xl'
+            />
+        </Center>  :
         <Box h='fit-content' maxW='100%'>
             <Image src='https://http2.mlstatic.com/D_NQ_721257-MLA52405202542_112022-OO.webp' alt='coverImg' w='100%' h='100%'/>
             <Box m='3%'>
                 <Heading size='md' marginBottom='2%'>Most Recent</Heading>
                 <Flex wrap='wrap' justify='space-around'>
-                    {data.map(prod=>(
+                    {data?.map(prod=>(
                         <Card key={prod._id}
                             title={prod.title}
                             price={prod.price}
@@ -36,6 +42,7 @@ export default function Home() {
                 <Button m='7%'>Load More</Button>
             </Center>
         </Box>
-        )
     }
+    </>
+    )
 }
