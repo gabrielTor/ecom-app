8d9cf62e83d0fcf8db29a21963a313894391b356
gabrielTor <gabriel_torres2012@yahoo.com>
2022-11-16 21:47:47 -0300

    passed current routes to redux

--- client/src/Redux/apiSlice.js
+++ client/src/Redux/apiSlice.js
@@ -5,17 +5,84 @@ export const apiSlice = createApi({
     tagTypes: ['Products', 'User'],
     endpoints: builder => ({
         getProducts: builder.query({
-            query: () => '/products',
+            query: (page) => `/products?page=${page}`,
+            providesTags: ['Products']
+        }),
+        searchProducts: builder.query({
+            query: (value) => `/search?search=${value}`,
             providesTags: ['Products']
         }),
         getProductById: builder.query({
             query: (id) => `/info/${id}`,
             providesTags: ['Products']
-        })
+        }),
+        getCategories: builder.query({
+            query: () => '/categories'
+        }),
+        getUsers: builder.query({
+            query: () => '/users',
+            providesTags: ['Users']
+        }),
+        loginUser: builder.mutation({
+            query: (user) => ({
+                url: '/registerOrLogin',
+                method: 'POST',
+                body: user
+            }),
+            invalidatesTags: ['User']
+        }),
+        postProduct: builder.mutation({
+            query: (obj) => ({
+                url: '/postProduct',
+                method: 'POST',
+                body: obj
+            }),
+            invalidatesTags: ['Products']
+        }),
+        logoutUser: builder.mutation({
+            query: (user) => ({
+                url: '/logout',
+                method: 'PUT',
+                body: user
+            }),
+            invalidatesTags: ['User']
+        }),
+        updateUser: builder.mutation({
+            query: (value) => ({
+                url: '/updateUser',
+                method: 'PUT',
+                body: value
+            }),
+            invalidatesTags: ['User']
+        }),
+        addFavorite: builder.mutation({
+            query: (value) => ({
+                url: '/logout',
+                method: 'PUT',
+                body: value
+            }),
+            invalidatesTags: ['User']
+        }),
+        removeFavorite: builder.mutation({
+            query: (value) => ({
+                url: '/logout',
+                method: 'PUT',
+                body: value
+            }),
+            invalidatesTags: ['User']
+        }),
     })
 })
 
 export const {
     useGetProductsQuery,
-    useGetProductByIdQuery
+    useGetProductByIdQuery,
+    useGetCategoriesQuery,
+    useGetUsersQuery,
+    usePostProductMutation,
+    useLoginUserMutation,
+    useLogoutUserMutation,
+    useUpdateUserMutation,
+    useAddFavoriteMutation,
+    useRemoveFavoriteMutation
 } = apiSlice
