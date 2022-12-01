eda2c9602316d8e57215cc1abc11c8d872e32ab3
gabrielTor <gabriel_torres2012@yahoo.com>
2022-12-01 19:17:47 -0300

    ux/ui fixes

--- client/src/components/Profile/features/MySellings.js
+++ client/src/components/Profile/features/MySellings.js
@@ -22,16 +22,16 @@ export default function MySellings(props) {
                 <Container key={listing._id} bg='green.100'>
                     <Heading size='md' m='1%' noOfLines={3}>{listing.title}</Heading>
                     <Flex justify={['space-between', 'space-evenly']}>
-                        <Image src={listing.image[0].url} alt='myListings' h='8rem'/>
+                        <Image src={listing.image[0].url} alt='myListings' h={['5rem', '6rem', '7rem', '8rem']}/>
                         <Flex direction='column'>
                             <Text>Selling at: ${listing.price}</Text>
                             <Text>Condition: {listing.used ? 'Used' : 'New'}</Text>
                             <Text>Amount Sold: {listing.sold}</Text>
                             <Text>Current Stock: {listing.stock}</Text>
                         </Flex>
-                        <Flex direction='column'>
+                        <Flex direction='column' justify='space-evenly'>
                             <Button variant='link'>Edit<EditIcon m='5%'/></Button>
-                            <Link as='Button' fontWeight='bold' href={`info/${listing._id}`}>Preview</Link>
+                            <Link fontWeight='bold' href={`info/${listing._id}`}>Preview</Link>
                         </Flex>
                     </Flex>
                 </Container>
