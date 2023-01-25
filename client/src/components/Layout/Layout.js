0157d43c6821b830661b910531634e1572a9c30a
gabrielTor <gabriel_torres2012@yahoo.com>
2023-01-24 23:02:21 -0300

    minH for outlet

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,18 +1,20 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
-import {Box} from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import { useLocation } from 'react-router-dom'
 
 export default function Layout() {
 
-  const {pathname} = useLocation()
+  const { pathname } = useLocation()
 
   return (
     <Box w='100%' bg='gray.100'>
-        <Header/>
-        <Outlet/>
-        {pathname !== '/messages' ? <Footer/> : null}
+      <Header />
+      <Box minH='18rem'>
+        <Outlet />
+      </Box>
+      {pathname !== '/messages' ? <Footer /> : null}
     </Box>
   )
 }
